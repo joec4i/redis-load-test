56e3cdab666bcc0f22f49cbd0de81375f973a9aa
Joe Cai <joe.cai@bigcommerce.com>
2020-11-23 15:18:00 +1100

    Add -keep-alive

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 func main()  {
 	connections := flag.Int("c", 10, "The number of clients to start")
 	addr := flag.String("addr", "10.133.136.177:63793", "The redis address")
+	keepAliveMS := flag.Int("keep-alive", 50, "The number of milliseconds to hold each connection")
 
 	flag.Parse()
 
@@ -30,26 +31,28 @@ func main()  {
 					DB:       0,  // use default DB
 				})
 
-				time.Sleep(50 * time.Millisecond)
-
 				readKey := "test:key:" + strconv.Itoa(rand.Intn(5000))
 				writeKey := "test:key:" + strconv.Itoa(rand.Intn(5000))
 
 				val, err := rdb.Get(ctx, readKey).Result()
 				if err != nil {
-					fmt.Println("#", clientNumber, "reading", readKey, ":", err)
-					continue
+					fmt.Println("#", clientNumber, "error reading", readKey, ":", err)
 				} else {
 					fmt.Println("#", clientNumber, "read", readKey, ":", len(val))
 				}
 
 				value := randstr.Hex(rand.Intn(1024))
 
-				err = rdb.Set(ctx, writeKey, value, 0).Err()
+				err = rdb.Set(ctx, writeKey, value, 5 * time.Minute).Err()
 				if err != nil {
 					fmt.Println("#", clientNumber, "error writing", writeKey, ":", err)
 				} else {
 					fmt.Println("#", clientNumber, "write", writeKey, ":", len(value))
+				}
+
+				if *keepAliveMS > 0 {
+					fmt.Println("#", clientNumber, "holding the connection for ", *keepAliveMS, "milliseconds")
+					time.Sleep(time.Duration(*keepAliveMS) * time.Millisecond)
 				}
 
 				err = rdb.Close()
